0bb0d62af81180bcb9eed31ca6c4e0d9b48193d1
nataliafrancisca <nathifrancisca@gmail.com>
2024-09-18 23:22:32 -0300

    feat: get username and date to show at the header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+'use server';
+import Image from "next/image";
+import DialogForm from "./components/dialogForm/DialogForm";
+import { getUserName } from "./storage/storage";
+import { getFormattedCurrentDate } from "./utils/date-formatted";
+
+export default async function Home() {
+
+  const userName = await getUserName();
+  const date = getFormattedCurrentDate();
+
   return (
-    <main>
-      <h1>teste</h1>
+    <main id="page__home">
+      {!userName && <DialogForm />}
+      <header>
+        <nav>
+          <Image src="logo.svg" alt="company logo" height={36} width={150} />
+          {userName && <p>Bem-vindo de volta, {userName}</p>}
+          <span>{date}</span>
+        </nav>
+      </header>
     </main>
   );
 }
